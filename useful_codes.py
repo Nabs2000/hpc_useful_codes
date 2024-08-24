d4278e74a7bdd33091e5ebc3d0810ae2a055f8d1
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2024-08-24 15:12:09 -0700

    Remove unnecessary Python file

--- useful_codes.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Chunking CSV files
-def chunk_csv_file(csv_fp: str, chunk_size: int):
-  chunks = []
-  for chunk in pd.read_csv(csv_fp, chunksize=chunk_size):
-    chunks.append(chunk)
-  return pd.concat(chunks)
-    
